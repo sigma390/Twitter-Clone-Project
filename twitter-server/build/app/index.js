f05b29cf7d635aedf8269ff6a929a4b29796628f
Omkar Patil <git21cs072-t@students.git.edu>
2024-01-13 20:11:31 +0530

    Other CHanges

---
+++ twitter-server/build/app/index.js
@@ -0,0 +1,45 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.initServer = void 0;
+const server_1 = require("@apollo/server");
+const express4_1 = require("@apollo/server/express4");
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+function initServer() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const graphqlServer = new server_1.ApolloServer({
+            typeDefs: `type Query{
+            sayHello:String,
+            sayHellotome(name:String):String
+
+        
+        }`,
+            resolvers: {
+                Query: {
+                    sayHello: () => `hello from graphql server`,
+                    sayHellotome: (parent, { name }) => `hey ${name}`
+                }
+            },
+        });
+        const app = (0, express_1.default)();
+        app.use(body_parser_1.default.json());
+        yield graphqlServer.start();
+        // Specify the path where we'd like to mount our server
+        app.use('/graphql', (0, cors_1.default)(), express_1.default.json(), (0, express4_1.expressMiddleware)(graphqlServer));
+        return app;
+    });
+}
+exports.initServer = initServer;
