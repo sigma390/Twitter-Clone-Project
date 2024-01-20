8a6ea362ff2d64619b5230ae3196366f09c3efca
Omkar Patil <git21cs072-t@students.git.edu>
2024-01-20 17:28:48 +0530

    Token data extraction is working great

--- twitter-server/src/app/user/resolvers.ts
+++ twitter-server/src/app/user/resolvers.ts
@@ -1,6 +1,18 @@
+import axios from "axios";
+
 const queries = {
     verifyGoogleToken:async (parent:any,{token}:{token:string})=>{
-        return token;
+        const googleToken = token; //google token
+        //url of google
+        const googleBaseUrl = new URL("https://oauth2.googleapis.com/tokeninfo");
+        googleBaseUrl.searchParams.set("id_token",googleToken);
+        //call to auth using axios and getting data
+        const {data} = await axios.get(googleBaseUrl.toString(),{
+            responseType:'json'
+        })
+        console.log(data)
+        return "ok"
+        
     },
 }
 export const resolvers = {queries}
